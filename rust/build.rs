e15e1eb7d559c6fb9943707c22190c26c080d423
Delton Ding <dsh0416@gmail.com>
2025-05-30 02:58:34 +0800

    ???

--- rust/build.rs
+++ rust/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if cfg!(target_os = "windows") {  
+    if std::env::var("CARGO_CFG_TARGET_OS").map_or(false, |v| v == "windows") {
         println!("cargo:rustc-link-lib=wevtapi");
     }
 }
